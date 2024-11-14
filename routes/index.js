6255364138b88baa0fc3672ccb98d03909135c1e
Jérémie Perret <jeremie.perret@bluewin.ch>
2024-11-14 14:06:42 +0100

    index router parse automatically json

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
-var express = require('express');
+var express = require("express");
+app = express();
 var router = express.Router();
 
+app.use(express.json());
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+    res.render("index", { title: "Express" });
 });
 
 module.exports = router;
